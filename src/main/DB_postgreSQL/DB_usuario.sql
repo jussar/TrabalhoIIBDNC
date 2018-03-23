65522e3ef945312de6468c40c4b7e41f0b8d7610
jussara <aras.strong8@gmail.com>
2018-03-23 20:38:49 -0300

    add DB do postgre para usuario

---
+++ src/main/DB_postgreSQL/DB_usuario.sql
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Jussara
+ * Created: 23/03/2018
+ */
+
+create table usuario(
+
+id SERIAL,
+email varchar(20),
+nome varchar(20),
+senha varchar(20),
+primary key (senha)
+
+)
